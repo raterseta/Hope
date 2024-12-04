2c6c1333f46b6fe980a6447cb8b8e720e9cc325d
qurreee <114373469+qurreee@users.noreply.github.com>
2024-12-04 09:49:53 +0700

    BottomNavComposable update, relocate MainActivity and PostComposable

--- app/src/main/java/com/example/hope/ui/composables/post/PostComposable.kt
+++ app/src/main/java/com/example/hope/ui/composables/post/PostComposable.kt
@@ -1,4 +1,4 @@
-package com.example.hope.ui.composables
+package com.example.hope.ui.composables.post
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -26,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
