cb00baa59bd32bb582b8a6d8b6a7f3d3f5aa835a
qurreee <114373469+qurreee@users.noreply.github.com>
2024-12-10 19:56:31 +0700

    PostData oke

--- app/src/main/java/com/example/hope/ui/composables/post/PostData.kt
+++ app/src/main/java/com/example/hope/ui/composables/post/PostData.kt
@@ -1,7 +1,6 @@
 package com.example.hope.ui.composables.post
 
 import androidx.compose.ui.graphics.painter.Painter
-import com.example.hope.ui.pages.register.UserData
 
 data class PostData(
     val postID: String,
@@ -9,5 +8,6 @@ data class PostData(
     val postP: Painter,
     val title: String,
     val description: String,
-    val isBookmarked: Boolean
+    val isBookmarked: Boolean,
+    val timestamp: Long = System.currentTimeMillis()
 )
