bc90ec42ec7dbc5ee104cc41b83037a1cf20ee8d
farelrakhadzakwan <114372861+farelrakhadzakwan@users.noreply.github.com>
2024-12-11 21:53:08 +0700

    mbenahin authentication google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,5 +97,11 @@ dependencies {
 
     implementation("io.coil-kt:coil-compose:2.4.0")
 
+    // Google Auth libraries
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
 }
 
